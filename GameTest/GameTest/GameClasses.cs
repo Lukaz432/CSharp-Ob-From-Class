bc39ede90a4fda599173b8b1b893cf55e8ae8c88
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-26 17:03:03 +0300

    Added-Room-Class

--- GameTest/GameTest/GameClasses.cs
+++ GameTest/GameTest/GameClasses.cs
@@ -50,5 +50,47 @@ namespace GameTest
                 _value = aValue;
             }
         }
+
+        public class Room : Thing
+        {
+            private int _n;
+            private int _s;
+            private int _w;
+            private int _e;
+
+            public Room(string aName, string aDescription, int aN, int aS, int aW, int aE)
+                : base(aName, aDescription)
+                    {
+                    _n = aN;
+                    _s = aS;
+                    _w = aW;
+                    _e = aE;
+                }
+            
+
+            public int N
+            {
+                get { return _n; }
+                set { _n = value; }
+            }
+
+            public int S
+            {
+                get { return _s; }
+                set { _s = value; }
+            }
+
+            public int W
+            {
+                get { return _s; }
+                set { _s = value; }
+            }
+
+            public int E
+            {
+                get { return _e; }
+                set { _e = value; }
+            }
+        }
     }
 }
